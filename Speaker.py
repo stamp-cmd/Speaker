50afbdeed17d7712525411c3401a5a44f8cd64a0
Kantanat-Stamp <76716475+stamp-cmd@users.noreply.github.com>
2021-10-01 09:55:13 +0700

    Update file and add requirements.txt

--- Speaker.py
+++ Speaker.py
@@ -5,7 +5,9 @@ from colorama import init, Fore, Style #for colored-text, and  bright text
 import pyfiglet #for making Title
 from time import sleep #for waiting
 
-init(autoreset=True, convert=True) #Initialize colorama library
+clear = "cls"
+
+init(autoreset=True, convert=True) #Initialize colorama library for windows
 
 Title = pyfiglet.figlet_format('Speaker', justify="center")#Declare variable Title to string
 print(Fore.CYAN + Style.BRIGHT + Title) #Print in varaible Title with cyan color
@@ -20,25 +22,25 @@ print(Fore.RED + Style.BRIGHT + '6. Quit to Quit program') #Info about "Quit" co
 print(Fore.RED + Style.BRIGHT + 'Red for system output. Yellow for warn. Green for command') #Color-text Info
 
 sleep(5) #Wait 5 seconds
-system('cls') #Clear the terminal system('clear') for linux
+system(clear) #Clear the terminal system('clear') for linux
 
 message = "" #Set variable 'message' to empty or ''
 
 while True: #Repeat until quit
     command = input(Fore.RED + '>>> ' + Fore.GREEN) #Input command and Declare variable 'message'
     if (command == "Write" or command == "write"):#Detect command "Write", "write"
-        system('cls') #Clear the terminal system('clear') for linux
+        system(clear) #Clear the terminal system('clear') for linux
         message = input(Fore.RED + 'message>>> ' + Fore.RESET) #Declare variable 'message' to string
         print(Fore.RED + 'Rewrite successfully') #Alert 'Rewrite successfully with red color
     elif (command == "Read" or command == "read"): #Detect command "Read", "read"
-        system('cls') #Clear the terminal system('clear') for linux
+        system(clear) #Clear the terminal system('clear') for linux
         print(Fore.RED + message) #Print variable
         print(Fore.RED + 'If nothing happen that because the message is empty') #Read Warning
         init()# initialize pyttsx3
         pyttsx3.speak(message)# pyttsx3 speak variable 'message'
         pyttsx3.init().runAndWait() # pyttsx3 Run and wait
     elif (command == "Reset" or command == "reset"):
-        system('cls') #Clear the terminal system('clear') for linux
+        system(clear) #Clear the terminal system('clear') for linux
         message = "" #Set variable 'message' to empty or ''
         print(Fore.RED + 'Reset successfully') # Alert 'Reset successfully' with red color
     elif (command == "Note" or command == "note"):# Detect command "Note", "note"
